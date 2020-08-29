92d2cd44d087935e2afaf47a7ca6cfc14544c61e
imws <877126478@qq.com>
2020-08-29 17:38:16 +0800

    把本地工程添加到git

---
+++ git-test-project/src/org/test/git/FirstClass.java
@@ -0,0 +1,15 @@
+package org.test.git;
+
+/**
+ * @author imws
+ * @date 2020/8/29 -17:30
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
